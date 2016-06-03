7db3747f795c8fd9971c6035e13db51fa43f13c5
smit thakkar <smitthakkar96@gmail.com>
2016-06-03 01:06:31 -0400

    merge commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
-var app = angular.module('punchit',['punchit.factory','ngCookies']);
+var app = angular.module('punchit',['punchit.factory','ngCookies','ngRoute']);
+
+
 
 app.controller('postsController',['$scope','$cookies','PostMan','UrlService',function($scope,PostMan,UrlService,$scope,$cookies){
-        
+
 }]);
