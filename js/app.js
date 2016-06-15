8858007af015f3ba4a4f31c723643c1ca6e88e2c
smit thakkar <smit@smits-MacBook-Air.local>
2016-06-15 01:51:04 -0400

    made some changes

--- js/app.js
+++ js/app.js
@@ -73,7 +73,7 @@ app.controller('postsController',['$scope','$cookies','PostMan','UrlService','au
     if (which == 'one') {
         if (punchers1.indexOf(id) == -1) {
           punchers1.push(id)
-          if (punchers2.indexOf(id) != 1) {
+          if (punchers2.indexOf(id) != -1) {
             punchers2.splice(id)
           }
         }
